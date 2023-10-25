26937b2ec6ac460a495262d7f3858cb95b5d6372
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-10-25 12:04:22 +0200

    correzzione components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { WeatherDisplayComponent } from './components/weather-display/weather-display.component';
-import { CurrentWeatherComponent } from './components/weather-display/current-weather/current-weather.component';
-import { ForecastComponent } from './components/weather-display/forecast/forecast.component';
-import { CitySelectorComponent } from './components/city-selector/city-selector.component';
 import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
 import { ErrorComponent } from './components/error/error.component';
 import { LoadingComponent } from './components/loading/loading.component';
@@ -18,9 +15,6 @@ import { HomeComponent } from './components/home/home.component';
     AppComponent,
     NavbarComponent,
     WeatherDisplayComponent,
-    CurrentWeatherComponent,
-    ForecastComponent,
-    CitySelectorComponent,
     WeatherDetailsComponent,
     ErrorComponent,
     LoadingComponent,
